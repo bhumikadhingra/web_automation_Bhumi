0b355135cdad170174414cbf4c6281a430e9728a
Bhumika Dhingra <51711116+bhumikadhingra@users.noreply.github.com>
2019-09-07 22:43:42 +0530

    Add files via upload

---
+++ ATG_Internship.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from selenium import webdriver
+import time
+
+def test_case_1():
+    browser.get('https://www.atg.party')
+    time.sleep(3)
+    login = browser.find_element_by_link_text('Login')
+    login.click()
+    time.sleep(0.5)
+    email = browser.find_element_by_id('email')
+    email.send_keys('hello@atg.world')
+    password = browser.find_element_by_id('password')
+    password.send_keys('Pass@123')
+    sign = browser.find_element_by_class_name('btns-loger')
+    sign.click()
+    time.sleep(7)
+
+def test_case_2():
+    test_case_1()
+    browser.get('https://www.atg.party/article')
+    time.sleep(3)
+    title = browser.find_element_by_id('title')
+    title.send_keys('Final Test Case')
+    desc = browser.find_element_by_class_name('fr-element.fr-view')
+    desc.send_keys('This is my final test case.')
+    time.sleep(0.5)
+    post = browser.find_elements_by_id('featurebutton')
+    post[-1].click()
+    time.sleep(7)
+    
+    
+path = r'D:\PycharmProjects\Coding_Elements\Web Development\chromedriver.exe'
+browser = webdriver.Chrome(path)
+test_case_2()
+  
+
+
+# In[ ]:
+
+
+
+
