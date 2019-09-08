b1140cb896d54e7d5d53ddd7cd7789d905994543
Ubuntu <ubuntu@ip-172-31-33-50.us-east-2.compute.internal>
2019-09-08 12:15:32 +0000

    time increased

--- ATG_Internship.py
+++ ATG_Internship.py
@@ -9,30 +9,30 @@ import time
 
 def test_case_1():
     browser.get('https://www.atg.party')
-    time.sleep(3)
+    time.sleep(5)
     login = browser.find_element_by_link_text('Login')
     login.click()
-    time.sleep(0.5)
+    time.sleep(1)
     email = browser.find_element_by_id('email')
     email.send_keys('hello@atg.world')
     password = browser.find_element_by_id('password')
     password.send_keys('Pass@123')
     sign = browser.find_element_by_class_name('btns-loger')
     sign.click()
-    time.sleep(7)
+    time.sleep(8)
 
 def test_case_2():
     test_case_1()
     browser.get('https://www.atg.party/article')
-    time.sleep(3)
+    time.sleep(5)
     title = browser.find_element_by_id('title')
     title.send_keys('Final Test Case')
     desc = browser.find_element_by_class_name('fr-element.fr-view')
     desc.send_keys('This is my final test case.')
-    time.sleep(0.5)
+    time.sleep(2)
     post = browser.find_elements_by_id('featurebutton')
     post[-1].click()
-    time.sleep(7)
+    time.sleep(8)
     
    
 options = webdriver.ChromeOptions()
