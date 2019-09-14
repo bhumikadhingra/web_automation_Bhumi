667501b5e16f8aa5009421d9c1f2fe2fee33cd34
bhumikadhingra <51711116+bhumikadhingra@users.noreply.github.com>
2019-09-14 12:12:22 +0530

    modified again at 12:12

--- ATG_Internship.py
+++ ATG_Internship.py
@@ -24,7 +24,7 @@ def test_case_1():
 
 def test_case_2():
     test_case_1()
-    browser.get('http://www.atg.party/article/)
+    browser.get('http://www.atg.party/article/')
     time.sleep(5)
     title = browser.find_element_by_id('title')
     title.send_keys('Final Test Case')
